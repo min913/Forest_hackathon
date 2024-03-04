11feff18baf803bd9037c0e3d5bf21c50e2cbcee
min913 <123183864+min913@users.noreply.github.com>
2024-03-04 19:01:35 +0900

    Update and rename Healing prediction to Healing prediction.py

---
+++ Empty model/Healing prediction.py
@@ -0,0 +1,33 @@
+class HealthEffectPredictor:
+    def __init__(self, height, weight, gender, program):
+        self.height = height
+        self.weight = weight
+        self.gender = gender
+        self.program = program
+    
+    def validate_input(self):
+        try:
+            float(self.height)
+            float(self.weight)
+        except ValueError:
+            return False
+        if self.gender not in ['남성', '여성']:
+            return False
+        if self.program not in ['등산로', '숲길', '재배', '치유']:
+            return False
+        return True
+    
+    def predict_health_effect(self):
+        if self.validate_input():
+            return '36kcal'
+        else:
+            return 'error'
+    
+    @classmethod
+    def prompt_and_predict(cls, height, weight, gender, program):
+        instance = cls(height, weight, gender, program)
+        return instance.predict_health_effect()
+
+
+HealthEffectPredictor.prompt_and_predict(188, 88, '여성', '치유')
+
