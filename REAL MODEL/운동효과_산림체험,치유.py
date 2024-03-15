845b8ad43d2d3974d7333ddd4c76e221982b014e
min913 <123183864+min913@users.noreply.github.com>
2024-03-15 18:22:20 +0900

    Update 운동효과_산림체험,치유.py

--- REAL MODEL/운동효과_산림체험,치유.py
+++ REAL MODEL/운동효과_산림체험,치유.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import pandas as pd
 import numpy as np
@@ -12,109 +7,6 @@ import datetime
 import numpy as np
 import os
 
-
-# In[91]:
-
-
-hth_sp = pd.read_csv(r"C:\Users\user\Desktop\산림데이터 공모전\데이터\건강효과_측정.csv")
-
-
-# In[61]:
-
-
-hth_sp
-
-
-# In[92]:
-
-
-col=['대상자키','대상자성별코드','대상자연령','대상자최소체지방량','대상자최대체지방량','대상자체중']
-
-
-# In[93]:
-
-
-hth_sp=hth_sp[col]
-
-
-# In[94]:
-
-
-hth_sp = hth_sp.iloc[1:]
-
-
-# In[95]:
-
-
-hth_sp
-
-
-# In[96]:
-
-
-hth_sp['대상자최소체지방량'] = pd.to_numeric(hth_sp['대상자최소체지방량'])
-hth_sp['대상자최대체지방량'] = pd.to_numeric(hth_sp['대상자최대체지방량'])
-hth_sp['대상자연령'] = pd.to_numeric(hth_sp['대상자연령'])
-hth_sp['대상자키'] = pd.to_numeric(hth_sp['대상자키'])
-hth_sp['대상자체중'] = pd.to_numeric(hth_sp['대상자체중'])
-hth_sp['체지방량_차이'] = hth_sp['대상자최대체지방량'] - hth_sp['대상자최소체지방량']
-
-
-# In[97]:
-
-
-hth_sp=hth_sp.drop(columns=['대상자최소체지방량','대상자최대체지방량'])
-
-
-# In[98]:
-
-
-hth_sp
-
-
-# In[99]:
-
-
-hth_sp.columns  = ['키','성별','나이','체중','차이']
-
-
-# In[100]:
-
-
-hth_sp=hth_sp.drop_duplicates()
-
-
-# In[101]:
-
-
-result_df = hth_sp.groupby(['키', '나이', '성별','체중'], as_index=False).agg({'차이':'mean'})
-
-
-# In[102]:
-
-
-result_df['차이']=result_df['차이']*7.8
-
-
-# In[107]:
-
-
-result_df['차이'].round(2)
-
-
-# In[110]:
-
-
-result_df['차이']
-
-
-# In[104]:
-
-
-type(result_df['체중'][0])
-
-
-# In[111]:
 
 
 def find_closest_match(df, height, age, gender, weight):
@@ -144,19 +36,8 @@ def find_closest_match(df, height, age, gender, weight):
     return 50.12
 
 
-# 사용자 입력 예시
-height_input = 160
-age_input = 20
-weight_input = 54
-gender_input = 'F'
-
-# 함수 실행 및 결과 출력
-difference = find_closest_match(result_df,height_input, age_input, gender_input,weight_input )
-difference=difference.round(2)
-print(f"찾은 '차이' 값: {difference}")
 
 
-# In[ ]:
 
 
 
