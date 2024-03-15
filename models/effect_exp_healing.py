0bc0e168f1f65c1f755936d364f5495aa17477e8
Suebin Kim <k.sue3028@gmail.com>
2024-03-16 03:24:54 +0900

    refactor: format files

--- models/effect_exp_healing.py
+++ models/effect_exp_healing.py
@@ -1,44 +1,28 @@
-
-import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import datetime
-import numpy as np
-import os
-
 
 
 def find_closest_match(df, height, age, gender, weight):
     # 필터링된 데이터프레임을 가중치로 정렬하는 함수
     def sort_by_weight_difference(filtered_df):
-        filtered_df['체중 차이'] = np.abs(filtered_df['체중'] - weight)
-        return filtered_df.sort_values(by='체중 차이').iloc[0]['차이']
-    
+        filtered_df["체중 차이"] = np.abs(filtered_df["체중"] - weight)
+        return filtered_df.sort_values(by="체중 차이").iloc[0]["차이"]
+
     # 키, 나이, 성별이 일치하는 데이터
-    match = df[(df['키'] == height) & (df['나이'] == age) & (df['성별'] == gender)]
+    match = df[(df["키"] == height) & (df["나이"] == age) & (df["성별"] == gender)]
     if not match.empty:
         return sort_by_weight_difference(match)
-    
+
     # 키와 나이가 일치하는 데이터
-    match = df[(df['키'] == height) & (df['나이'] == age)]
+    match = df[(df["키"] == height) & (df["나이"] == age)]
     if not match.empty:
         return sort_by_weight_difference(match)
-    
+
     # 키가 동일한 데이터에서 나이와 체중 차이가 최소인 데이터
-    match = df[df['키'] == height]
+    match = df[df["키"] == height]
     if not match.empty:
-        match['나이 차이'] = np.abs(match['나이'] - age)
-        match = match.sort_values(by=['나이 차이', '체중 차이'])
+        match["나이 차이"] = np.abs(match["나이"] - age)
+        match = match.sort_values(by=["나이 차이", "체중 차이"])
         if not match.empty:
-            return match.iloc[0]['차이']
-    
+            return match.iloc[0]["차이"]
+
     return 50.12
-
-
-
-
-
-
-
-
--- models/recom_exp_healing.py
+++ models/recom_exp_healing.py
@@ -1,93 +1,110 @@
-import random
 from sentence_transformers import SentenceTransformer, util
 import pandas as pd
+import os
 from datetime import datetime
 import math
-import os.path
-from . import schemas, models
-import logging
 
 my_path = os.path.abspath(os.path.dirname(__file__))
-logger = logging.getLogger("uvicorn")
-logger.setLevel(logging.INFO)
+
 # 데이터 불러오기
-prefer_exp_path = os.path.join(my_path, "datasets/program_preferences.csv")
-prefer_exp = pd.read_csv(prefer_exp_path)
+prefer_exp_data = os.path.join(my_path, "datasets/exp_preference_data.csv")
+prefer_healing_data = os.path.join(my_path, "datasets/healing_preference_data.csv")
+program_list_data = os.path.join(my_path, "../datasets/program_list.csv")
 
-program_df_path = os.path.join(my_path, "datasets/programs.csv")
-program_df = pd.read_csv(program_df_path)
-prgm = list(program_df["제목"])
-
-sentenceTransformer = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 
 # 유저 정보 입력 및 유저와 유사한 기존 검색 사용자 찾기
-def __user_info(df, user=models.User):
-    age = datetime.today().year - user.birth_year
+def __get_selected_preferences(prefer_df, birth_year, gender, job, has_children):
+    age = datetime.today().year - birth_year
     age_range = str(math.floor(age / 10) * 10) + "대"
-    df["matching_count"] = df.apply(
+    prefer_df["matching_count"] = prefer_df.apply(
         lambda row: sum(
             [
-                row["성별"] == user.gender,
-                row["직업"] == user.job,
-                row["자녀여부"] == user.has_children,
+                row["성별"] == gender,
+                row["직업"] == job,
+                row["자녀여부"] == has_children,
                 row["연령대"] == age_range,
             ]
         ),
         axis=1,
     )
-    top_matches = df.sort_values(by="matching_count", ascending=False).head(10)
+    top_matches = prefer_df.sort_values(by="matching_count", ascending=False).head(10)
     top_indexes = top_matches.index.tolist()
-    df.drop(columns=["matching_count"], inplace=True)
-    selected_preferences = [prefer_exp["제목"][i] for i in top_indexes]
+    prefer_df.drop(columns=["matching_count"], inplace=True)
+    selected_preferences = [prefer_df["제목"][i] for i in top_indexes]
     return selected_preferences
 
 
 # 문장 유사도 계산 -> 프로그램 간 유사도 계산산
-def __calculate_similarity(embedding1, embedding2):    
+def __calculate_similarity(embedding1, embedding2):
     # 코사인 유사도 계산
     cosine_similarity = util.pytorch_cos_sim(embedding1, embedding2)
     return cosine_similarity.item()
 
-def get_embedding(sentence):
-    return sentenceTransformer.encode(sentence)
 
-program_df["제목_임베딩"] = program_df.apply(lambda x: get_embedding(x["제목"]), axis=1)
+def __get_embedding(sentence):
+    return model.encode(sentence)
+
+
+program_list_data["name_embedding"] = program_list_data.apply(
+    lambda x: __get_embedding(x["name"]), axis=1
+)
 
 
 # 최대 유사도 찾아서 index 계산
-def __max_similarity(sentence,):
+def __max_similarity(
+    embedding,
+    embeddingList,
+):
     similarities = []
 
-    sentence_embed = get_embedding(sentence)
-
     # 각 선호도 항목과의 유사도 계산
-    for index, e in enumerate(program_df["제목_임베딩"]):
-        similarity = __calculate_similarity(sentence_embed, e)
-        similarities.append((similarity, index, e))
+    for index, e in enumerate(embeddingList):
+        similarity = __calculate_similarity(embedding, e)
+        similarities.append(
+            (
+                similarity,
+                index,
+            )
+        )
 
     # 유사도 기준으로 내림차순 정렬 후 상위 8개 선택
     top_8_similarities = sorted(similarities, reverse=True)[:8]
     index_list = []
-    for sim, index, pref in top_8_similarities:
-        print(f"Index: {index}")
+    for __sim__, index in top_8_similarities:
         index_list.append(index)
 
     return index_list
 
 
 # 최종 추출 함수
-def recommend(user=models.User, ):
-    selected_preferences = __user_info(prefer_exp, user)
+def __get_outputs(prefer_df, list_data, birth_year, gender, job, has_children):
+    selected_preferences = __get_selected_preferences(
+        prefer_df, birth_year, gender, job, has_children
+    )
     outputs = []
     for u in selected_preferences:
         # 각 선호도에 대한 가장 유사한 프로그램 인덱스 찾기
-        output_indexes = __max_similarity(u)
+        output_indexes = __max_similarity(u, list_data)
         # 가장 유사한 프로그램의 인덱스를 사용하여 프로그램 선택
-
         for index in output_indexes:
-            program_row = program_df.loc[index,:]
-            program = schemas.ProgramMini(id=index, name=program_row["제목"], rate=round(random.random()*6), category="등산로")
-            outputs.append(program)
-
+            outputs.append(list_data[index])
     return outputs
+
+
+def run(birth_year, gender, job, has_children):
+    exp_idx = program_list_data["category"] == "산림체험"
+    healing_idx = program_list_data["category"] == "산림 치유원"
+
+    exp_df = program_list_data.loc[exp_idx, :]
+    healing_df = program_list_data.loc[healing_idx, :]
+
+    exp_outputs = __get_outputs(
+        prefer_exp_data, exp_df, birth_year, gender, job, has_children
+    )
+    healing_outputs = __get_outputs(
+        prefer_healing_data, healing_df, birth_year, gender, job, has_children
+    )
+
+    return pd.concat([exp_outputs, healing_outputs])
